28f209543757a37c428b37ef17fee20f885e4df1
Sartxi <sean.archibeque@gmail.com>
2025-05-08 09:25:35 -0600

    other hero variant

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -35,12 +35,42 @@ function decorateBg(block) {
   }
 }
 
+function decorateCustomColumns(block) {
+  if (block.className.includes('columns')) {
+    const colsClass = block.className.split(' ').find((cls) => cls.includes('columns'));
+    const cols = colsClass.split('-');
+    cols.shift();
+    [...block.firstElementChild.children].forEach((child, index) => {
+      child.style.setProperty('width', `${cols[index]}%`);
+    });
+  }
+}
+
+function decorateContentMeta(block) {
+  const metas = block.querySelectorAll(':scope code');
+  metas.forEach((meta) => {
+    if (meta.textContent.includes('meta:')) {
+      const metaData = meta.textContent.split(':');
+      metaData.shift();
+      const [key, value] = metaData;
+      const content = meta.closest('div');
+      if (key.trim() === 'variant') {
+        content.classList.add(value.trim());
+      } else {
+        content.style.setProperty(key.trim(), value.trim());
+      }
+    } else {
+      meta.classList.add('code-content');
+    }
+  });
+}
+
 function parallaxBg(block) {
   if (block.classList.contains('parallax')) {
-    window.onscroll = () => {
+    window.addEventListener('scroll', () => {
       const offset = window.scrollY * 0.5;
       block.firstChild.style.inset = `calc(1px - ${offset}px) 50%`;
-    };
+    });
   }
 }
 
@@ -49,4 +79,6 @@ export default async function decorate(block) {
   const content = block.querySelector(':scope > div');
   content.className = 'content-wrapper';
   if (block.classList.contains('parallax')) parallaxBg(block);
+  decorateCustomColumns(block);
+  decorateContentMeta(block);
 }
