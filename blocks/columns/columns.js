cfa38000a81f873588605036707c75cd46367f9e
Sartxi <sean.archibeque@gmail.com>
2025-05-30 13:07:19 -0600

    add subheader block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,9 +2,18 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  // use block column widths from variant
+  let widths;
+  const customWidths = [...block.classList].filter((i) => i.includes('widths'));
+  if (customWidths?.length) {
+    widths = customWidths[0]?.split('-');
+    widths?.shift();
+  }
+
   // setup image columns
   [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
+    [...row.children].forEach((col, i) => {
+      if (widths) col.style.setProperty('--col-width', `${widths[i]}%`);
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
